8d7039417fba607f38345e56e2073d7cb26d0113
MoAlyousef <may642_2000@hotmail.com>
2021-10-29 15:18:36 +0300

    apply orphaned spacing distribution for Flex

--- FL_Flex.cpp
+++ FL_Flex.cpp
@@ -2,24 +2,26 @@
 
 #include <FL/Fl.H>
 
-WidgetVec::WidgetVec()
-    : buf(new Fl_Widget*[10])
-    , cap(10)
-    , len(0)
-{}
+WidgetVec::WidgetVec() : buf(new Fl_Widget *[10]), cap(10), len(0) {}
 
-size_t WidgetVec::size() const { return len; }
+size_t WidgetVec::size() const {
+    return len;
+}
 
-Fl_Widget* WidgetVec::at(size_t idx) { return buf[idx]; }
+Fl_Widget *WidgetVec::at(size_t idx) {
+    return buf[idx];
+}
 
-Fl_Widget* WidgetVec::operator[](size_t idx) { return at(idx); }
+Fl_Widget *WidgetVec::operator[](size_t idx) {
+    return at(idx);
+}
 
-void WidgetVec::push_back(Fl_Widget* w)
-{
+void WidgetVec::push_back(Fl_Widget *w) {
     if (len == cap) {
         cap = cap * 2;
-        Fl_Widget** new_buf = new Fl_Widget*[cap];
-        for (size_t i = 0; i < len; i++) new_buf[i] = buf[i];
+        Fl_Widget **new_buf = new Fl_Widget *[cap];
+        for (size_t i = 0; i < len; i++)
+            new_buf[i] = buf[i];
         delete[] buf;
         buf = new_buf;
     }
@@ -27,43 +29,33 @@ void WidgetVec::push_back(Fl_Widget* w)
     len++;
 }
 
-WidgetVec::~WidgetVec()
-{
+WidgetVec::~WidgetVec() {
     delete[] buf;
 }
 
 bool Fl_Flex::_debug = false;
 
-Fl_Flex::Fl_Flex(uchar direction)
-    : Fl_Group(0, 0, 0, 0, 0)
-{
+Fl_Flex::Fl_Flex(uchar direction) : Fl_Group(0, 0, 0, 0, 0) {
     type(direction);
     debug();
 }
 
-Fl_Flex::Fl_Flex(int w, int h, uchar direction)
-    : Fl_Group(0, 0, w, h, 0)
-{
+Fl_Flex::Fl_Flex(int w, int h, uchar direction) : Fl_Group(0, 0, w, h, 0) {
     type(direction);
     debug();
 }
 
-Fl_Flex::Fl_Flex(int x, int y, int w, int h, uchar direction)
-    : Fl_Group(x, y, w, h, 0)
-{
+Fl_Flex::Fl_Flex(int x, int y, int w, int h, uchar direction) : Fl_Group(x, y, w, h, 0) {
     type(direction);
     debug();
 }
 
-Fl_Flex::Fl_Flex(int x, int y, int w, int h, const char* label)
-    : Fl_Group(x, y, w, h, label)
-{
+Fl_Flex::Fl_Flex(int x, int y, int w, int h, const char *label) : Fl_Group(x, y, w, h, label) {
     type(ROW);
     debug();
 }
 
-void Fl_Flex::debug()
-{
+void Fl_Flex::debug() {
     // Color the rows and columns to help facilitate development.
     if (!_debug)
         return;
@@ -77,8 +69,7 @@ void Fl_Flex::debug()
     }
 }
 
-void Fl_Flex::resizeRow(int x, int y, int w, int h)
-{
+void Fl_Flex::resizeRow(int x, int y, int w, int h) {
     int cc = children();
     int padW = w - _margin * 2;
 
@@ -93,29 +84,38 @@ void Fl_Flex::resizeRow(int x, int y, int w, int h)
     // Precalculate remaining size to resize to
     // Calculate non-resizable width total
     for (int i = 0; i < cc; i++) {
-        Fl_Widget* c = child(i);
+        Fl_Widget *c = child(i);
 
         if (isSetSize(c)) {
             nrs += c->w();
         }
     }
 
+    // Orphaned space which is not evenly distributable
+    int osp = (padW - nrs) % (cc - (int)setsized.size());
+
     // Set children to shared width of remaining
     for (int i = 0; i < cc; i++) {
-        Fl_Widget* c = child(i);
+        Fl_Widget *c = child(i);
 
         if (isSetSize(c)) {
             c->resize(cx, y + _margin, c->w(), h - _margin * 2);
         } else {
-            c->resize(cx, y + _margin, (padW - nrs) / (cc - (int)setsized.size()), h - _margin * 2);
+            if (osp > 0) {
+                osp -= 1;
+                c->resize(cx, y + _margin, (padW - nrs) / (cc - (int)setsized.size()) + 1,
+                          h - _margin * 2);
+            } else {
+                c->resize(cx, y + _margin, (padW - nrs) / (cc - (int)setsized.size()),
+                          h - _margin * 2);
+            }
         }
 
         cx += c->w() + _pad;
     }
 }
 
-void Fl_Flex::resizeCol(int x, int y, int w, int h)
-{
+void Fl_Flex::resizeCol(int x, int y, int w, int h) {
     int cc = children();
     int padH = h - _margin * 2;
 
@@ -130,16 +130,19 @@ void Fl_Flex::resizeCol(int x, int y, int w, int h)
     // Precalculate remaining size to resize to
     // Calculate non-resizable height total
     for (int i = 0; i < cc; i++) {
-        Fl_Widget* c = child(i);
+        Fl_Widget *c = child(i);
 
         if (isSetSize(c)) {
             nrs += c->h();
         }
     }
 
+    // Orphaned space which is not evenly distributable
+    int osp = (padH - nrs) % (cc - (int)setsized.size());
+
     // Set children to shared width of remaining
     for (int i = 0; i < cc; i++) {
-        Fl_Widget* c = child(i);
+        Fl_Widget *c = child(i);
 
         if (isSetSize(c)) {
             c->resize(x + _margin, cy, w - _margin * 2, c->h());
@@ -148,16 +151,21 @@ void Fl_Flex::resizeCol(int x, int y, int w, int h)
             // This is very handy to allow resizable items to have negative height
             // allowing one on the top and bottom to center contents even if container
             // is too small.
-            c->resize(x + _margin, cy, w - _margin * 2,
-                (padH - nrs) / (cc - (int)setsized.size()));
+            if (osp > 0) {
+                osp -= 1;
+                c->resize(x + _margin, cy, w - _margin * 2,
+                          (padH - nrs) / (cc - (int)setsized.size()) + 1);
+            } else {
+                c->resize(x + _margin, cy, w - _margin * 2,
+                          (padH - nrs) / (cc - (int)setsized.size()));
+            }
         }
 
         cy += c->h() + _pad;
     }
 }
 
-void Fl_Flex::resize(int x, int y, int w, int h)
-{
+void Fl_Flex::resize(int x, int y, int w, int h) {
     Fl_Widget::resize(x, y, w, h);
 
     if (type() == COLUMN) {
@@ -167,14 +175,12 @@ void Fl_Flex::resize(int x, int y, int w, int h)
     }
 }
 
-void Fl_Flex::end()
-{
+void Fl_Flex::end() {
     Fl_Group::end();
     resize(x(), y(), w(), h());
 }
 
-void Fl_Flex::setSize(Fl_Widget* w, int size)
-{
+void Fl_Flex::setSize(Fl_Widget *w, int size) {
     w->resize(0, 0, size, size);
 
     for (size_t i = 0; i < setsized.size(); i++) {
@@ -186,8 +192,7 @@ void Fl_Flex::setSize(Fl_Widget* w, int size)
     setsized.push_back(w);
 }
 
-bool Fl_Flex::isSetSize(Fl_Widget* w)
-{
+bool Fl_Flex::isSetSize(Fl_Widget *w) {
     for (size_t i = 0; i < setsized.size(); i++) {
         if (w == setsized.at(i)) {
             return true;
